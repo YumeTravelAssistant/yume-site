92d2d1a158dcc3fd687fe7db57ec84d05ca504fc
YumeTravelAssistant <progettogiappone@gmail.com>
2026-07-16 16:56:34 +0200

    Update blog-config.js

--- blog/assets/blog-config.js
+++ blog/assets/blog-config.js
@@ -8,4 +8,13 @@ window.YUME_JOURNAL_CONFIG = Object.freeze({
   supabasePublishableKey: "sb_publishable_Z5S66pZ85I3WlGuJDArJhA_QuXhKP51",
   siteUrl: "https://yume-travel.com",
   pageSize: 24,
+
+  /**
+   * Incolla qui il link pubblico reale del canale YUME ON BOARD.
+   * Esempio: https://whatsapp.com/channel/XXXXXXXXXXXXXXXX
+   * Se resta vuoto, il box viene nascosto automaticamente.
+   */
+  whatsappChannelUrl: "",
+
+  shareCampaign: "yume_journal",
 });
